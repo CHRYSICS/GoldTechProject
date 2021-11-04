d6a94979c3d77f079a932a404e40b8d749e0ed9a
chrysics <eckersoc@oregonstate.edu>
2021-11-03 17:53:22 -0700

    updated routing for main app (dashboard and login components)

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    render() {
+        return (
+            <div className="wrapper">
+                <h1>GoldTech Adminstration API</h1>
+                <BrowserRouter>
+                    <Switch>
+                        <Route path="/login">
+                            <Login />
+                        </Route>
+                        <Route path='/dashboard'>
+                            <Dashboard />
+                        </Route>
+                    </Switch>
+                </BrowserRouter>
+            </div>
+        );
+    }
+    
 }
 
 export default App;
