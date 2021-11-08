9c2cff977d6d37a1e4d03d7bc37b0b2be32ea353
chrysics <eckersoc@oregonstate.edu>
2021-11-08 15:13:21 -0800

    updated extension to create new user and offer option to upload coupon file

--- Clippings/background.js
+++ Clippings/background.js
@@ -1,6 +1,24 @@
-let color = '#3aa757';
+// request to make new user, assign returned id to chrome storage
+function generateUser() {
+    console.log("requesting to generate new user in database");
+    var url = 'http://localhost:5000/users';
+    var requestOptions = {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({title: 'Generate new user thru POST request'})
+    }
+    fetch(url, requestOptions)
+    .then(res => res.json())
+    .then(data => {
+        let id = data.id;
+        chrome.storage.sync.set({"id": id}, function() {
+            console.log("New User:",data.id);
+        });
+        chrome.storage.sync.set({"codes": []}, function() {
+            console.log("empty list of codes generated in chrome storage")
+        });
+    });
+}
 
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({ color });
-    console.log('Default background color set to %cgreen', `color: ${color}`);
-});
+// Generate new user in mongodb database when extension is installed
+chrome.runtime.onInstalled.addListener(generateUser);
--- Clippings/popup.js
+++ Clippings/popup.js
@@ -1,26 +1,28 @@
-// Initialize button with user's preferred color
-let changeColor = document.getElementById("changeColor");
-// Initialize button with user's preferred color
-let changeColor = document.getElementById("changeColor");
+// get user element
+let user = document.getElementById("user");
+let uploadButton = document.getElementById("upload");
 
-chrome.storage.sync.get("color", ({ color }) => {
-    changeColor.style.backgroundColor = color;
+chrome.storage.sync.get("id", ({ id }) => {
+    user.innerHTML = id;
 });
 
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        function: setPageBackgroundColor,
-    });
-});
-
-// The body of this function will be executed as a content script inside the
-// current page
-function setPageBackgroundColor() {
-    chrome.storage.sync.get("color", ({ color }) => {
-        document.body.style.backgroundColor = color;
-    });
+// load file uploaded, parse by whitespace, adds to chrome storage
+function upLoadCouponFile() {
+    var reader = new FileReader();
+    var [file] = document.querySelector("input[type=file]").files;
+    reader.addEventListener('load', function() {
+        let newCodes = reader.result.split(" ");
+        chrome.storage.sync.get({codes: []}, (data) => {
+            alert(data.codes);
+            newCodes.forEach(function(code) {
+                if (!(code in data.codes)) {
+                    data.codes.push(code);
+                }
+            });
+            chrome.storage.sync.set({codes: data.codes});
+        });
+    }, false);
+    reader.readAsText(file);
 }
+
+uploadButton.addEventListener("click", upLoadCouponFile);
